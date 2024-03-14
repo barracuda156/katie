a5e29c80dd70299028eebd1dd1230b7e8157cfb7
Ivailo Monev <xakepa10@gmail.com>
2024-03-14 02:05:26 +0200

    remove unused qt_pixmapForBrush() function
    Signed-off-by: Ivailo Monev <xakepa10@gmail.com>

--- src/gui/painting/qbrush.cpp
+++ src/gui/painting/qbrush.cpp
@@ -56,12 +56,6 @@ static const uchar *qt_patternForBrush(int brushStyle)
     return pat_tbl[brushStyle - Qt::Dense1Pattern];
 }
 
-QPixmap qt_pixmapForBrush(int brushStyle)
-{
-
-    return QBitmap::fromData(QSize(8, 8), qt_patternForBrush(brushStyle), QImage::Format_MonoLSB);
-}
-
 Q_GUI_EXPORT QImage qt_imageForBrush(int brushStyle)
 {
     return QImage(qt_patternForBrush(brushStyle), 8, 8, 1, QImage::Format_MonoLSB);
